35417d382f860efd03088c7ba7e4fbf9750424be
rolaman <rolamanbuss@yandex.ru>
2024-12-26 11:32:41 +0400

    Uniswap: migrate to nil.js usage

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,26 +6,28 @@ import "@typechain/hardhat";
 import * as dotenv from "dotenv";
 import type { HardhatUserConfig } from "hardhat/config";
 
-// Basic
-import "./tasks/basic/create-wallet";
-
-// Currency Tasks
-import "./tasks/currency/info";
-import "./tasks/currency/mint-wallet";
-import "./tasks/currency/deploy-currency";
-
-// Core Tasks
-import "./tasks/uniswap/pair/get-reserves";
-import "./tasks/uniswap/pair/mint";
-import "./tasks/uniswap/pair/burn";
-import "./tasks/uniswap/pair/swap";
-import "./tasks/uniswap/factory/get-pair";
-import "./tasks/uniswap/factory/create-pair";
-import "./tasks/uniswap/factory/deploy-factory";
-
-// Demo Tasks
-import "./tasks/uniswap/demo-router";
-import "./tasks/uniswap/demo-router-sync";
+// Typescript cannot be compiled before running `npx hardhat compile`
+// Should be uncommented after running `npx hardhat compile`
+// // Basic
+// import "./tasks/basic/create-wallet";
+//
+// // Currency Tasks
+// import "./tasks/currency/info";
+// import "./tasks/currency/mint-wallet";
+// import "./tasks/currency/deploy-currency";
+//
+// // Core Tasks
+// import "./tasks/uniswap/pair/get-reserves";
+// import "./tasks/uniswap/pair/mint";
+// import "./tasks/uniswap/pair/burn";
+// import "./tasks/uniswap/pair/swap";
+// import "./tasks/uniswap/factory/get-pair";
+// import "./tasks/uniswap/factory/create-pair";
+// import "./tasks/uniswap/factory/deploy-factory";
+//
+// // Demo Tasks
+// import "./tasks/uniswap/demo-router";
+// import "./tasks/uniswap/demo-router-sync";
 
 dotenv.config();
 
